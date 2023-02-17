d7af0d2170c46124617ddc2a0951b2ad61523698
manasisingh26 <120970136+manasisingh26@users.noreply.github.com>
2023-02-17 17:38:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import '../styles/App.css';
+
+import React from "react";
+import BusHireFooter from "./Components/Bus hire Main Page/Bus Hire Footer Section/BusHireFooter";
+import SubFooter from "./Components/Bus hire Main Page/Sub Footer Section/SubFooter";
+import Navbar from "./Components/Navbar/Navbar";
+import Routes from "./Routes/Routes";
+
 const App = () => {
-
-
   return (
-    <div id="main">
+    <div style={{ overflowX: "hidden" }}>
+      <Navbar />
+      <Routes />
+      <SubFooter />
+      <BusHireFooter />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
